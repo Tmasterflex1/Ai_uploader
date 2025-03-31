50ed02626b7edf6dcd2df1312c5cd4dc5ee5679a
Tmasterflex1 <Bigtsantiago@icloud.com>
2025-03-30 23:29:25 -0400

    Added the button and instsalled prettier adn eslint extensions

---
+++ Desktop/Node js/ai_uploader/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Source_Sans_3 as FontSans } from "next/font/google";
+import "./globals.css";
+
+const fontSans = FontSans({
+  variable: "--font-sans",
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+
+export const metadata: Metadata = {
+  title: "Ai Uploader",
+  description: "Ai Uploader is an app that allows you to upload pdfs and get a summary of the content.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${fontSans.variable} font-sans antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
